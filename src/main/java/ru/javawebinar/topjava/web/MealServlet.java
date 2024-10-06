3ecf63ee7a158358dde91ad0ad212ab104b57980
Andrey Kamenskiy <kamenskiy@krista.ru>
2024-10-06 23:44:56 +0300

    hw1

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.MealTo;
 import ru.javawebinar.topjava.util.MealsUtil;
 
@@ -9,21 +10,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.slf4j.LoggerFactory.getLogger;
+import static ru.javawebinar.topjava.web.AppInitializer.CALORIES_PER_DAY_ATTRIBUTE;
+import static ru.javawebinar.topjava.web.AppInitializer.MEALS_ATTRIBUTE;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(MealServlet.class);
     private static final String MEALS_TO_ATTRIBUTE = "mealsToList";
 
     @Override
+    @SuppressWarnings("unchecked")
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
             IOException {
-        List<MealTo> mealToList = MealsUtil.filteredByStreams(
-                AppGlobalContext.getMeals(), null, null,
-                AppGlobalContext.getCaloriesPerDay()
-        );
+        List<Meal> meals = Collections.emptyList();
+        int caloriesPerDay = 0;
+        try {
+            meals = (List<Meal>) getServletContext().getAttribute(MEALS_ATTRIBUTE);
+            caloriesPerDay = (int) getServletContext().getAttribute(CALORIES_PER_DAY_ATTRIBUTE);
+        } catch (Exception ignore) {
+        }
+        List<MealTo> mealToList = MealsUtil.filteredByStreams(meals, null, null, caloriesPerDay)
+                .stream()
+                .sorted(Comparator.comparing(MealTo::getDateTime))
+                .collect(Collectors.toList());
         log.debug("Created {} MealTo", mealToList.size());
         request.setAttribute(MEALS_TO_ATTRIBUTE, mealToList);
         log.debug("forward to meals");
--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -14,9 +14,9 @@ public class UserServlet extends HttpServlet {
     private static final Logger log = getLogger(UserServlet.class);
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
         log.debug("forward to users");
         request.getRequestDispatcher("/users.jsp").forward(request, response);
-//        response.sendRedirect("users.jsp");
     }
 }
