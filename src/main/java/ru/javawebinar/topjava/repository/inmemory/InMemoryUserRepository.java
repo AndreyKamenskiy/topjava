d339d60952631f2c64b599a5cee8fc7780e611b8
Andrey Kamenskiy <kamenskiy@krista.ru>
2024-10-15 09:13:46 +0300

    remove email maps from InMemoryUserRepository
    seems in makes implementation not atomic

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -1,11 +1,11 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import com.sun.istack.internal.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
-import ru.javawebinar.topjava.util.exception.RepositoryException;
 
 import java.util.Comparator;
 import java.util.List;
@@ -18,28 +18,13 @@ import java.util.stream.Collectors;
 public class InMemoryUserRepository implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
 
-    private final Map<Integer, User> idToUser = new ConcurrentHashMap<>();
-
-    // email must be uniq. Tested in demo
-    private final Map<String, User> emailToUser = new ConcurrentHashMap<>();
-    private final Map<Integer, String> idToEmail = new ConcurrentHashMap<>();
+    private final Map<Integer, User> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
-
-    //todo: test for atomic. think about...
 
     @Override
     public boolean delete(int id) {
         log.info("delete {}", id);
-        if (idToUser.containsKey(id)) {
-            String email = idToEmail.get(id);
-            boolean error = emailToUser.remove(email) == null ||
-                    idToEmail.remove(email) == null ||
-                    idToUser.remove(id) == null;
-            if (error) {
-                throw new RepositoryException("User deleting error");
-            }
-        }
-        return false;
+        return repository.remove(id) != null;
     }
 
     @Override
@@ -48,52 +33,33 @@ public class InMemoryUserRepository implements UserRepository {
         if (user.isNew()) {
             int id = counter.incrementAndGet();
             user.setId(id);
-            String email = user.getEmail();
-            if (emailToUser.containsKey(email)) {
-                throw new IllegalArgumentException(
-                        String.format("Saving user error. Email %s is already exists", email));
-            }
-            idToUser.put(user.getId(), user);
-            emailToUser.put(email, user);
-            idToEmail.put(id, email);
+            repository.put(user.getId(), user);
             return user;
         }
-        int id = user.getId();
-        if (idToUser.containsKey(id)) {
-            String oldEmail = idToEmail.get(id);
-            if (oldEmail == null || oldEmail.isEmpty()) {
-                throw new RepositoryException("Saving user error. Unable get old email value or value is empty");
-            }
-            String newEmail = user.getEmail();
-            if (!oldEmail.equals(newEmail)) {
-                idToEmail.put(id, newEmail);
-                emailToUser.remove(oldEmail);
-                emailToUser.put(newEmail, user);
-            }
-            return idToUser.put(id, user);
-        }
-        return null;
+        return repository.computeIfPresent(user.getId(), (id, oldUser) -> user);
     }
 
     @Override
     public User get(int id) {
         log.info("get {}", id);
-        return idToUser.get(id);
+        return repository.get(id);
     }
 
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return idToUser.values()
+        return repository.values()
                 .stream()
                 .sorted(Comparator.comparing(User::getName))
                 .collect(Collectors.toList());
     }
 
     @Override
-    public User getByEmail(String email) {
+    public User getByEmail(@NotNull String email) {
         log.info("getByEmail {}", email);
-        // to simplify solution we returning real user bean. Email must not be changed.
-        return emailToUser.getOrDefault(email, null);
+        return repository.values()
+                .stream()
+                .filter(user -> email.equals(user.getEmail()))
+                .findFirst().orElse(null);
     }
 }
