c546d52cdd9184f49b4749f6bd6d8715bbdc1868
Andrey Kamenskiy <kamenskiy@krista.ru>
2024-10-17 00:18:37 +0300

    fix style error in MealsUtil

--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -47,8 +47,11 @@ public class MealsUtil {
 
     public static List<MealTo> getFilteredTos(Collection<Meal> meals, int caloriesPerDay, LocalTime startTime,
                                               LocalTime endTime) {
-        return filterByPredicate(meals, caloriesPerDay, meal -> DateTimeUtil.isBetweenHalfOpen(meal.getTime(),
-                startTime, endTime));
+        return filterByPredicate(
+                meals,
+                caloriesPerDay,
+                meal -> DateTimeUtil.isBetweenHalfOpen(meal.getTime(), startTime, endTime)
+        );
     }
 
     private static List<MealTo> filterByPredicate(Collection<Meal> meals, int caloriesPerDay, Predicate<Meal> filter) {
