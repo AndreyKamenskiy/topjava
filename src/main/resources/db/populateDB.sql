1c278ae4390b0a57a312463dce10e9a0a3d6c21f
Andrey Kamenskiy <kamenskiy@krista.ru>
2024-10-21 00:43:33 +0300

    done hw3 base part

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,6 @@
-DELETE FROM user_role;
+-- DELETE FROM user_role; -- will be cascade deleted
+-- DELETE FROM meals  -- don't needed because of cascade deleting meals with users
+-- noinspection SqlWithoutWhere
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -10,3 +12,11 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, calories, description)
+VALUES (100000, '2024-10-20 09:51:00.000000 +03:00', 500, 'завтрак юзера'),
+       (100000, '2024-10-20 13:51:00.000000 +03:00', 500, 'обед юзера'),
+       (100000, '2024-10-20 19:51:00.000000 +03:00', 500, 'ужин юзера'),
+       (100001, '2024-10-20 12:00:00.000000 +03:00', 500, 'смузи админа'),
+       (100001, '2024-10-20 06:00:00.000000 +03:00', 500, 'кофе админа'),
+       (100001, '2024-10-20 21:30:00.000000 +03:00', 500, 'успокоительное для админа');
