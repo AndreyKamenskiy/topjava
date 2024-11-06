c4209d76350e87d707d44c13333dd3fd1b5e2b61
Andrey Kamenskiy <rybinsk@mail.ru>
2024-11-07 02:08:24 +0300

    fix service tests
    remove redundant methods overrides which only calls super method

--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
@@ -7,49 +7,4 @@ import ru.javawebinar.topjava.service.UserServiceTest;
 
 @ActiveProfiles(profiles = Profiles.DATAJPA, resolver = ActiveDbProfileResolver.class)
 public class DataJpaUserServiceTest extends UserServiceTest {
-
-    @Override
-    public void create() {
-        super.create();
-    }
-
-    @Override
-    public void duplicateMailCreate() {
-        super.duplicateMailCreate();
-    }
-
-    @Override
-    public void delete() {
-        super.delete();
-    }
-
-    @Override
-    public void deletedNotFound() {
-        super.deletedNotFound();
-    }
-
-    @Override
-    public void get() {
-        super.get();
-    }
-
-    @Override
-    public void getNotFound() {
-        super.getNotFound();
-    }
-
-    @Override
-    public void getByEmail() {
-        super.getByEmail();
-    }
-
-    @Override
-    public void update() {
-        super.update();
-    }
-
-    @Override
-    public void getAll() {
-        super.getAll();
-    }
 }
