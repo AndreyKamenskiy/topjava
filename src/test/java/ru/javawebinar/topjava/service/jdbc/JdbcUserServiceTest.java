2b9d30186e6ae592e684326883e2fcbcc7939c70
Andrey Kamenskiy <rybinsk@mail.ru>
2024-11-21 00:36:13 +0300

    remove DirtiesContext in JdbcUserServiceTest

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
@@ -1,22 +1,33 @@
 package ru.javawebinar.topjava.service.jdbc;
 
+import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static ru.javawebinar.topjava.Profiles.JDBC;
 import static ru.javawebinar.topjava.UserTestData.*;
 
 @ActiveProfiles(JDBC)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class JdbcUserServiceTest extends AbstractUserServiceTest {
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    @Before
+    public void setup() {
+        Optional.ofNullable(cacheManager.getCache("users")).ifPresent(Cache::clear);
+    }
 
     @Test
     public void dirtyCacheFirstPart() {
